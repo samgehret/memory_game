8108f7d50be37ac7a98f1530c1fafb4ab8eca164
Sam Gehret <johngehret@Sams-MacBook-Pro.local>
2017-12-28 12:24:05 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!")
+	}
+	else {
+		alert("Sorry please try again!")
+	}
+}
