4d0984e570cd14e0a2b73ec109865d34c427088a
Sam Gehret <johngehret@Sams-MacBook-Pro.local>
2018-01-01 13:41:34 -0500

    created game logic 2

--- js/main.js
+++ js/main.js
@@ -4,23 +4,28 @@ var cards = ["queen","queen","king","king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
+var checkForMatch = function() {
 if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0]===cardsInPlay[1]){
+if (cardsInPlay[0]===cardsInPlay[1]){
 		alert("You found a match!")
 	}
 	else {
 		alert("Sorry please try again!")
 	}
 }
+}
+
+var flipCard = function(cardId) {
+
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
