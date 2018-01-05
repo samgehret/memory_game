b4bbd9a13adbfefb6ed1c4fd7b4fd637423787a8
Sam Gehret <johngehret@Sams-MBP.fios-router.home>
2018-01-05 15:51:47 -0500

    finished game logic

--- js/main.js
+++ js/main.js
@@ -26,8 +26,10 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+	
+
 if (cardsInPlay.length === 2) {
-if (cardsInPlay[0]===cardsInPlay[1]){
+if (cardsInPlay[0].rank===cardsInPlay[1].rank){
 		alert("You found a match!")
 	}
 	else {
@@ -36,19 +38,31 @@ if (cardsInPlay[0]===cardsInPlay[1]){
 }
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
 
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].suit);
 console.log(cards[cardId].cardImage);
+this.setAttribute("src", cards[cardId].cardImage);
 
-cardsInPlay.push(cards[cardId].rank);
+cardsInPlay.push(cards[cardId]);
 
 checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+createBoard();
+//flipCard(0);
+//flipCard(2);
 
 
 
